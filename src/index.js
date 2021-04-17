b4b3bccd4395447d3fd86f6531a66908c154d46f
trinamae12 <maetrina12@gmail.com>
2021-04-17 21:51:49 +0800

    Redirect to welcome page if route does not exist

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Router>
+    {/* <App /> */}
+    <Switch>
+      <Route path="/" exact component={App} />
+      <Route render={() => <Redirect to="/" />} />
+    </Switch>
+  </Router>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
